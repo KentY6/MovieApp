0e6a4a17fc3a8c5bac75a95dda993101a65ad81f
KentY <e6ony.ivory@gmail.com>
2023-10-30 19:12:26 +0900

    showページ設定

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,10 +6,15 @@ class MoviesController < ApplicationController
     apiUrl = "https://api.themoviedb.org/3/movie/popular?api_key=#{ENV['API_KEY']}&language=ja"
     responce = client.get(apiUrl)
     res_json = JSON.parse(responce.body)
-    @movies = res_json
-    puts @movies
+    @movies = res_json['results']
   end
 
   def show
+    client = HTTPClient.new
+    apiUrl = "https://api.themoviedb.org/3/movie/#{params[:id]}?api_key=#{ENV['API_KEY']}&language=ja"
+    responce = client.get(apiUrl)
+    res_json = JSON.parse(responce.body)
+    @movie = res_json
+
   end
 end
---
+++ app/models/movie.rb
@@ -0,0 +1,6 @@
+class Movie < ApplicationRecord
+    attribute :title, :string
+    attribute :overView, :string
+    attribute :id, :integer
+    attribute :img, :string
+end
