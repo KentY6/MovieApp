63a01a6fb5e204bceff3842da733c70a420585a2
KentY <e6ony.ivory@gmail.com>
2023-10-31 15:34:11 +0900

    検索機能追加

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   require 'httpclient'
+  protect_from_forgery with: :null_session
 
   def index
     client = HTTPClient.new
@@ -15,6 +16,13 @@ class MoviesController < ApplicationController
     responce = client.get(apiUrl)
     res_json = JSON.parse(responce.body)
     @movie = res_json
+  end
 
+  def search
+    client = HTTPClient.new
+    apiUrl = "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['API_KEY']}&language=ja&page=1&query=#{params[:text]}"
+    responce = client.get(apiUrl)
+    res_json = JSON.parse(responce.body)
+    @movies = res_json['results']
   end
 end
